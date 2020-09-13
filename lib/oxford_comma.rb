b7037ecfd088c847531b8376b956d5b917c74b8c
Moose Davis <moosedavis2011@gmail.com>
2020-09-13 16:20:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 require "pry"
 def oxford_comma(array)
+  binding.pry
   if array.length < 3
     array.join(" and ")
-    binding.pry
   else
     grocery_list = array.pop
     array.join(", ") + ", and " + grocery_list
