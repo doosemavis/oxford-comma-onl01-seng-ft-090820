5aa95a5f4638bc98b1be06f191235c1952d281e6
Moose Davis <moosedavis2011@gmail.com>
2020-09-13 16:04:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   if array.length < 3
     array.join(" and ")
   else
-    word = array.pop
-    array.join(", ") + ", and " + word
+    grocery_list = array.pop
+    array.join(", ") + ", and " + grocery_list
   end 
 end
 
